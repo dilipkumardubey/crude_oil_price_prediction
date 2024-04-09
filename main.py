4c5f1ef687ade3963c32d155489206a917b118a7
Dilip-Kumar-Dubey <todilipdubey@gmail.com>
2024-04-09 22:28:48 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 # # Check for custom log. This will be printed everytime when main.py will be executed.
 # logger.info("Welcome to our custom log.")
 
-# Test stage_01_data_ingestion pipeline
+# Data Ingestion stage
 STAGE_NAME = "Data Ingestion stage"
+
 try:
     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nX=================X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+# Data Validation stage
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nX=================X")
 except Exception as e:
